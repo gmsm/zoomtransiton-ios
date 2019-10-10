9a137d2df3770173c27146e66d47d678e106d635
Glauco <glaucomoraes@bloov.com>
2019-10-10 16:02:20 -0300

    Create UIColorExtensions and ZoomTransitionManager

---
+++ ZoomTransition/ZoomTransition/UIColorExtension.swift
@@ -0,0 +1,17 @@
+//
+//  UIColorExtension.swift
+//  ZoomTransition
+//
+//  Created by Glauco Moraes on 10/10/19.
+//  Copyright © 2019 Glauco. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let sunsetOrange = UIColor(red: 255/255, green: 89/255, blue: 94/255, alpha: 1)
+    static let sunGlow = UIColor(red: 255/255, green: 202/255, blue: 58/255, alpha: 1)
+    static let yellowGreen = UIColor(red: 138/255, green: 201/255, blue: 38/255, alpha: 1)
+    static let cynaCornflowerBlue = UIColor(red: 25/255, green: 130/255, blue: 196/255, alpha: 1)
+    static let darkLavender = UIColor(red: 106/255, green: 76/255, blue: 147/255, alpha: 1)
+}
---
+++ ZoomTransition/ZoomTransition/ZoomTransitionManager.swift
@@ -0,0 +1,14 @@
+//
+//  ZoomTransitionManager.swift
+//  ZoomTransition
+//
+//  Created by Glauco Moraes on 10/10/19.
+//  Copyright © 2019 Glauco. All rights reserved.
+//
+
+import UIKit
+
+struct ZoomTransitionManager {
+    var cardView: UIView
+    var cardViewFrame: CGRect
+}
